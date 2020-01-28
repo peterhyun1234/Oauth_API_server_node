750daad54db95b6ba144a24c89bbdd20336b8a0e
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-01-28 15:08:24 +0900

    add next project for testing oauth as client

---
+++ test_client/oauth_test/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        oauth_test
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+</style>
+
